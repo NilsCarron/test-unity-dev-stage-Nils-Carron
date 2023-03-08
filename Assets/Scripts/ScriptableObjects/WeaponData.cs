3f3c987e6d8d6998dcfe62b36ee4e81385d08108
NilsCarron <nilscarron@yahoo.fr>
2023-03-08 15:21:56 +0100

    Adding all the WeaponStats

--- Assets/Scripts/ScriptableObjects/WeaponData.cs
+++ Assets/Scripts/ScriptableObjects/WeaponData.cs
@@ -1,7 +1,29 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 [CreateAssetMenu(fileName = "WeaponData", menuName = "Data/WeaponData", order = 0)]
+
+
 public class WeaponData : ScriptableObject
 {
-    
+public enum WeaponRarity{Commun, Rare, Epique, Légendaire};
+public enum WeaponType{Principale, Secondaire};
+public enum WeaponReach {Mêlée, Pugilat, Distance}
+
+
+
+
+    public String WeaponName;
+    public Sprite Image;
+    public float Price;
+    public WeaponRarity Rarity;
+    public WeaponType Type;
+    public WeaponReach Reach;
+    public float Weight;
+    public float Damages;
+    public float Penetration;
+    public float Speed;
+    public float State;
+
+
 }
